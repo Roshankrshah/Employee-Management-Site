76c64925f6a6ba46dac5f13cff828c093fcb7068
Roshan <shahroshan0720@gmail.com>
2023-09-22 13:21:29 +0530

    DB connection setup

---
+++ Server/db/connect.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql');
+
+const connectDB = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.DB_PORT
+});
+
+module.exports = connectDB;
--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,21 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const connectDB = require('./db/connect');
 
 const app = express();
 
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
+
+connectDB.connect((err) => {
+    if (err) {
+        console.log(err.message);
+    }
+    console.log('DB ' + connectDB.state);
+});
 
 app.get('/',(req,res)=>{
     res.send('Starting the Employee Management Project');
